fd5c9c2802bade8785ddf452e1f6d48548d97d4c
igna31 <36173140+igna31@users.noreply.github.com>
2019-05-03 21:53:52 -0400

    Add files via upload

---
+++ js/WAcompile/WAcompile.js
@@ -0,0 +1,26 @@
+const isInNode = (typeof process !== 'undefined') && (typeof process.versions.node !== 'undefined')
+const isWabtDefined = (typeof WabtModule === 'undefined')
+if(isInNode){
+    console.log("You\'re running this file in NodeJS, This file is not compatible with NodeJS for now, please wait.")
+    process.exit(1);
+}
+if(isWabtDefined) {
+    var s = document.createElement("script")
+    s.src = 'https://cdn.jsdelivr.net/npm/wabt@latest/index.js';
+    document.head.appendChild(s);
+}
+
+const compileWebAssembly = (code, log = true, write_debug_names = true) => {
+    let arr = [];
+    for(let i = 0; i < code.length; i++) {
+        arr[i] = code.charCodeAt(i);
+    }
+    const bytes = new Uint8Array(arr);
+    let features = []
+    const modules = WabtModule().parseWat(code, bytes, features);
+    modules.resolveNames();
+    modules.validate(features);
+    const bin = modules.toBinary({log:log, write_debug_names:write_debug_names});
+    const lib = new WebAssembly.Instance(new WebAssembly.Module(bin.buffer),{}).exports;
+    return lib;
+}
